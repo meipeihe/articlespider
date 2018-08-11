b1d7929ea4e9f489fb478d3bb269d824afd3a377
Tim Zhou <Tim_Zhou@epam.com>
2018-08-11 12:02:46 +0800

    first commit

---
+++ articlespider/spiders/jobbole.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+class JobboleSpider(scrapy.Spider):
+    name = 'jobbole'
+    allowed_domains = ['blog.jobbole.com']
+    start_urls = ['http://blog.jobbole.com/110287']
+
+    def parse(self, response):
+        title = response.css(".entry-header>h1::text").extract()
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0]
+        praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        fav_nums = response.css(".bookmark-btn::text").extract()[0]
+        match_re = re.match(".*?(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+        else:
+            fav_nums = 0
+
+        comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = match_re.group(1)
+        else:
+            comment_nums = 0
+
+        content = response.css("div.entry").extract()[0]
+
+        tags = response.css("p.entry-meta-hide-on-mobile a::text").extract()[0]
+
+        pass
